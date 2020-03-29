f475f81e7e209b5a4dcde0eaf54f2e373819bf57
Aleksandr Kataev <akataev1819@gmail.com>
2020-03-29 18:00:49 +0100

    Changed static asset path

--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 const path = require('path');
 const express = require('express');
 const app = express();
-const publicPath = path.join(__dirname, '/client/build');
+const publicPath = path.join(__dirname, './client/build');
 const port = process.env.PORT || 5000;
 const bubbleSort = require('./routes/bubbleSort');
 
