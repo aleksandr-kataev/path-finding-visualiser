abe423e9bf7b33ae4f09e55dede723bbd2f63daf
Aleksandr Kataev <akataev1819@gmail.com>
2020-03-29 18:02:54 +0100

    debuging publicpath

--- server.js
+++ server.js
@@ -2,6 +2,7 @@ const path = require('path');
 const express = require('express');
 const app = express();
 const publicPath = path.join(__dirname, './client/build');
+console.log(publicPath);
 const port = process.env.PORT || 5000;
 const bubbleSort = require('./routes/bubbleSort');
 
