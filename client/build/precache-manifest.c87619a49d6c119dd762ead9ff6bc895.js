8ec4b648600b06364967907881b59fd42ce06ba6
Aleksandr Kataev <akataev1819@gmail.com>
2020-03-29 18:19:18 +0100

    Removed build from gitignore

---
+++ client/build/precache-manifest.c87619a49d6c119dd762ead9ff6bc895.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5c73541b64f0db3c37401256447b1936",
+    "url": "/index.html"
+  },
+  {
+    "revision": "09a372e663ce337248a5",
+    "url": "/static/css/main.5ecd60fb.chunk.css"
+  },
+  {
+    "revision": "1ccfd3c4749d5d9a3058",
+    "url": "/static/js/2.b64521c5.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.b64521c5.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "09a372e663ce337248a5",
+    "url": "/static/js/main.b5ab0c71.chunk.js"
+  },
+  {
+    "revision": "6f0234b4b87d585d06e8",
+    "url": "/static/js/runtime-main.cfae3202.js"
+  }
+]);
