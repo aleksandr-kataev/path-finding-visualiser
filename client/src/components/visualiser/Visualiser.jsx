d20350768430e4cb9e7d6097948887633dd786a2
Aleksandr Kataev <akataev1819@gmail.com>
2020-06-28 13:30:56 +0100

    The user can now draw and remove obstacles with a mouse

--- client/src/components/visualiser/Visualiser.jsx
+++ client/src/components/visualiser/Visualiser.jsx
@@ -5,8 +5,45 @@ import { Grid, Box } from "@material-ui/core";
 import { AlgoContext } from "../AlgoContext";
 
 const Visualiser = (props) => {
+  const [isLeftMouseDown, setIsLeftMouseDown] = useState(false);
+  const [isRightMouseDown, setIsRightMouseDown] = useState(false);
   const [algo, setAlgo] = useContext(AlgoContext);
-  const handleClick = (event) => {
+
+  const handleContextMenu = (event) => {
+    event.preventDefault();
+  };
+
+  const handleMouseDown = (event) => {
+    if (event.button === 0) {
+      setIsLeftMouseDown(true);
+    }
+    if (event.button === 2) {
+      setIsRightMouseDown(true);
+    }
+  };
+
+  const handleMouseUp = (event) => {
+    event.stopPropagation();
+    if (event.button === 0) {
+      setIsLeftMouseDown(false);
+      registerObstacle(event);
+    }
+    if (event.button === 2) {
+      setIsRightMouseDown(false);
+      removeObstacle(event);
+    }
+  };
+
+  const handleMouseMove = (event) => {
+    if (isLeftMouseDown) {
+      registerObstacle(event);
+    }
+    if (isRightMouseDown) {
+      removeObstacle(event);
+    }
+  };
+
+  const removeObstacle = (event) => {
     const x = Math.trunc(event.nativeEvent.offsetX / 25);
     const y = Math.trunc(event.nativeEvent.offsetY / 25);
     if (
@@ -15,13 +52,43 @@ const Visualiser = (props) => {
         (x === algo.start.x && y === algo.start.y)
       )
     ) {
-      setAlgo({
-        ...algo,
-        obstacles:
-          algo.obstacles.length < 1
-            ? [{ x, y }]
-            : [...algo.obstacles, { x, y }],
-      });
+      if (
+        algo.obstacles.findIndex(
+          (obstacle) => obstacle.x === x && obstacle.y === y
+        ) !== -1
+      ) {
+        setAlgo({
+          ...algo,
+          obstacles: algo.obstacles.filter(
+            (elem) => elem.x !== x || elem.y !== y
+          ),
+        });
+      }
+    }
+  };
+
+  const registerObstacle = (event) => {
+    const x = Math.trunc(event.nativeEvent.offsetX / 25);
+    const y = Math.trunc(event.nativeEvent.offsetY / 25);
+    if (
+      !(
+        (x === algo.end.x && y === algo.end.y) ||
+        (x === algo.start.x && y === algo.start.y)
+      )
+    ) {
+      if (
+        algo.obstacles.findIndex(
+          (obstacle) => obstacle.x === x && obstacle.y === y
+        ) === -1
+      ) {
+        setAlgo({
+          ...algo,
+          obstacles:
+            algo.obstacles.length < 1
+              ? [{ x: x, y: y }]
+              : [...algo.obstacles, { x: x, y: y }],
+        });
+      }
     }
   };
 
@@ -32,8 +99,13 @@ const Visualiser = (props) => {
       alignItems='center'
       justify='center'
     >
-      <Box onMouseDown={handleClick}>
-        <Canvas height={752} width={1877}></Canvas>
+      <Box
+        onMouseDown={handleMouseDown}
+        onMouseUp={handleMouseUp}
+        onMouseMove={handleMouseMove}
+        onContextMenu={handleContextMenu}
+      >
+        <Canvas height={751} width={1876}></Canvas>
       </Box>
     </Grid>
   );
